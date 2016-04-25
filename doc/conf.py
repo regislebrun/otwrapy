7e6bc2710efb497c3ad65a96c511b31cb9b736b0
Felipe Aguirre Martinez <aguirre@phimeca.com>
2016-04-25 17:30:04 +0200

    Add logos

--- doc/conf.py
+++ doc/conf.py
@@ -72,9 +72,11 @@ html_sidebars = {
     ]
 }
 html_theme_options = {
+    'logo': 'LogoPhiHaut.png',
     'logo_name': 'otwrapy',
     'description': 'Collection of tools that simplify the task of wrapping ' + 
-                   'external codes in a Python environment',
+                   'external codes in a Python environment. ' +
+                   'Proudly develloped at Phimeca engineering.',
     'github_user': 'felipeam86',
     'github_repo': 'otwrapy',
     'github_banner': True,
@@ -84,7 +86,7 @@ html_theme_options = {
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-# html_favicon = ''
+html_favicon = 'Icon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

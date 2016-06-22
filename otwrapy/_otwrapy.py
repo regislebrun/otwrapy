0b88c8ff131fc1b94d34e8cd0d95ae7f4803d79a
Felipe Aguirre Martinez <aguirre@phimeca.com>
2016-06-22 16:24:04 +0200

    Remove trailing logger name due to copy/paste

--- otwrapy/_otwrapy.py
+++ otwrapy/_otwrapy.py
@@ -92,7 +92,7 @@ def create_logger(logfile, loglevel=None):
     if loglevel is None:
         loglevel = logging.DEBUG
 
-    logger = logging.getLogger("vgp")
+    logger = logging.getLogger()
     logger.setLevel(loglevel)
 
     # ----------------------------------------------------------

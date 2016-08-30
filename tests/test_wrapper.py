3c91840703f6e8de27852459cb13bf79372a519f
Antoine Dumas <dumas@phimeca.com>
2016-08-30 15:14:14 +0200

    update tests

--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -11,7 +11,7 @@ def test_joblib():
     """Test joblib backend
     """
     # Joblib backend
-    model = otw.Parallelizer(Wrapper(where='phimeca', sleep=0.2),
+    model = otw.Parallelizer(Wrapper(sleep=0.2),
         backend='joblib', n_cpus=-1)
     Y_sample_joblib = model(X_sample)
     Y_mean_joblib = model(X_distribution.getMean())
@@ -20,7 +20,7 @@ def test_multiprocessing():
     """Test multiprocessing backend
     """
     # Multiprocessing backend
-    model = otw.Parallelizer(Wrapper(where='phimeca', sleep=0.2),
+    model = otw.Parallelizer(Wrapper(sleep=0.2),
         backend='multiprocessing', n_cpus=4)
     Y_sample_multiprocessing = model(X_sample)
     Y_mean_multiprocessing = model(X_distribution.getMean())
@@ -28,7 +28,7 @@ def test_multiprocessing():
 def test_ipython():
     """Test ipython backend
     """
-    model = otw.Parallelizer(Wrapper(where='phimeca', sleep=0.2),
+    model = otw.Parallelizer(Wrapper(sleep=0.2),
         backend='ipython')
     X = X_distribution.getSample(4)
     Y_sample_multiprocessing = model(X)
